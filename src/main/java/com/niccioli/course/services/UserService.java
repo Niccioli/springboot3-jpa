0934fc7158f8b6f4050c307fde7e878f5fc0888e
Rafael Niccioli <r.niccioli@gmail.com>
2025-08-30 12:58:56 -0300

    Exception handling - update

--- src/main/java/com/niccioli/course/services/UserService.java
+++ src/main/java/com/niccioli/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.niccioli.course.entities.User;
 import com.niccioli.course.repositories.UserRepository;
 import com.niccioli.course.services.exceptions.DatabaseException;
 import com.niccioli.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -46,9 +47,13 @@ public class UserService {
     }
 
     public User update(Long id, User user){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
